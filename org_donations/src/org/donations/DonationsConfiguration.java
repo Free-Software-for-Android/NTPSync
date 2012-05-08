a6230729f802653c545112bc8bf8d3306e239ea1
Dominik <dominik@dominikschuermann.de>
2012-05-08 13:03:41 +0300

    moved readme to wiki, configured donations lib

--- org_donations/src/org/donations/DonationsConfiguration.java
+++ org_donations/src/org/donations/DonationsConfiguration.java
@@ -24,13 +24,13 @@ public class DonationsConfiguration {
 
     /** Flattr */
 
-    public static final String FLATTR_PROJECT_URL = "http://code.google.com/p/ad-away/";
-    public static final String FLATTR_URL = "http://flattr.com/thing/369138/AdAway-Ad-blocker-for-Android";
+    public static final String FLATTR_PROJECT_URL = "http://code.google.com/p/ntp-sync/";
+    public static final String FLATTR_URL = "http://flattr.com/thing/669294/NTPSync";
 
     /** PayPal */
 
     public static final String PAYPAL_USER = "dominik@dominikschuermann.de";
-    public static final String PAYPAL_ITEM_NAME = "AdAway Donation";
+    public static final String PAYPAL_ITEM_NAME = "NTPSync Donation";
     public static final String PAYPAL_CURRENCY_CODE = "EUR";
 
     /** Google Play Store In-App Billing */
@@ -39,7 +39,7 @@ public class DonationsConfiguration {
     public static final String GOOGLE_PUBLIC_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAg8bTVFK5zIg4FGYkHKKQ/j/iGZQlXU0qkAv2BA6epOX1ihbMz78iD4SmViJlECHN8bKMHxouRNd9pkmQKxwEBHg5/xDC/PHmSCXFx/gcY/xa4etA1CSfXjcsS9i94n+j0gGYUg69rNkp+p/09nO9sgfRTAQppTxtgKaXwpfKe1A8oqmDUfOnPzsEAG6ogQL6Svo6ynYLVKIvRPPhXkq+fp6sJ5YVT5Hr356yCXlM++G56Pk8Z+tPzNjjvGSSs/MsYtgFaqhPCsnKhb55xHkc8GJ9haq8k3PSqwMSeJHnGiDq5lzdmsjdmGkWdQq2jIhKlhMZMm5VQWn0T59+xjjIIwIDAQAB";
     // mapping from the possible donations of 1,2,3,5,8 and 13 eur to your in-app items defined in
     // the publisher account of google play
-    public static final String[] GOOGLE_CATALOG = new String[] { "adaway.donation.1",
-            "adaway.donation.2", "adaway.donation.3", "adaway.donation.5", "adaway.donation.8",
-            "adaway.donation.13" };
+    public static final String[] GOOGLE_CATALOG = new String[] { "ntpsync.donation.1",
+            "ntpsync.donation.2", "ntpsync.donation.3", "ntpsync.donation.5", "ntpsync.donation.8",
+            "ntpsync.donation.13" };
 }
