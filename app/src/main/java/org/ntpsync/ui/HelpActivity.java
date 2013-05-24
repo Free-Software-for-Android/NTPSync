99007103c09226b84fa3391ad59a1ef1b9e7a124
Dominik Schürmann <dominik@dominikschuermann.de>
2013-05-24 18:09:06 +0200

    format

--- app/src/main/java/org/ntpsync/ui/HelpActivity.java
+++ app/src/main/java/org/ntpsync/ui/HelpActivity.java
@@ -65,9 +65,11 @@ public class HelpActivity extends FragmentActivity {
 
         DonationsFragment donationsFragment;
         if (BuildConfig.DONATIONS_GOOGLE) {
-            donationsFragment = DonationsFragment.newInstance(true, GOOGLE_PUBKEY, GOOGLE_CATALOG, false, null, null, null, false, null, null);
+            donationsFragment = DonationsFragment.newInstance(true, GOOGLE_PUBKEY, GOOGLE_CATALOG, false, null, null,
+                    null, false, null, null);
         } else {
-            donationsFragment = DonationsFragment.newInstance(false, null, null, true, PAYPAL_USER, PAYPAL_CURRENCY_CODE, getString(R.string.pref_paypal_donation_item), true, FLATTR_PROJECT_URL, FLATTR_URL);
+            donationsFragment = DonationsFragment.newInstance(false, null, null, true, PAYPAL_USER,
+                    PAYPAL_CURRENCY_CODE, getString(R.string.pref_paypal_donation_item), true, FLATTR_PROJECT_URL, FLATTR_URL);
         }
 
         ft.replace(R.id.help_donations_container, donationsFragment);
